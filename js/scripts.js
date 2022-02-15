bcab711af485667afbaf844422bbf80884a5936d
Alexander K <niktonezapomnil@gmail.com>
2022-02-16 04:48:23 +0500

    animations

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,29 @@ $(document).ready(function () {
         $(popupId).removeClass('js-popup-show');
         $('body').removeClass('no-scrolling');
     }
+
+    scrollWaypointInit($('.animateMe'));
 });
+
+function scrollWaypointInit(items, trigger) {
+    items.each(function() {
+        var element = $(this),
+            osAnimationClass = element.data("animation"),
+            osAnimationDelay = element.attr('data-animation-delay');
+
+        element.css({
+            '-webkit-animation-delay': osAnimationDelay,
+            '-moz-animation-delay': osAnimationDelay,
+            'animation-delay': osAnimationDelay
+        });
+
+        var trigger = (trigger) ? trigger : element;
+
+        trigger.waypoint(function() {
+            element.addClass('animated').addClass(osAnimationClass);
+        }, {
+            // triggerOnce: true,
+            offset: '80%'
+        });
+    });
+}
